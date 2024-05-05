4c96c82b75c7fd9240c99135bd596b31f7ca2102
a1elen <112384717+a1elen@users.noreply.github.com>
2024-05-05 21:16:57 +0800

    added weapons

--- js/items.js
+++ js/items.js
@@ -1,11 +1,12 @@
+items = {}
+
 item = {
     name: "Item",
     type: "item",
     sprite: 0
 }
 
-items = {}
-
+// Food
 items.food = Object.create(item);
 items.food.type = "food";
 items.food.hunger = 10;
@@ -21,3 +22,40 @@ items.food.apple.sprite = 73;
 items.food.meat = Object.create(items.food);
 items.food.meat.name = "Meat";
 items.food.meat.sprite = 50;
+
+items.food.bread = Object.create(items.food);
+items.food.bread.name = "Bread";
+items.food.bread.sprite = 74;
+
+// Weapons
+items.weapons = Object.create(item);
+items.weapons.type = "weapon";
+items.weapons.diceRolls = 1;
+items.weapons.diceSides = 1;
+items.weapons.damageType = "slash";
+items.weapons.quality = "Normal";
+items.weapons.material = "Iron";
+items.weapons.accuracy = 1;
+items.weapons.attackSpeed = 1;
+items.weapons.get = function() {
+    player.inventory.push(this);
+    addPopups("Picked up " + this.fullName, "white", player);
+    return true;
+};
+items.weapons.fullName = function() {
+    let name = this.quality+" "+this.material+" "+this.name;
+    return name;
+};
+
+function makeSword() {
+    materials = ["Copper", "Bronze", "Iron", "Silver", "Gold", "Steel"];
+
+    let matMap = new Map();
+    matMap.set(materials[0], 51);
+    matMap.set(materials[1], 52);
+    matMap.set(materials[2], 53);
+    matMap.set(materials[3], 55);
+    matMap.set(materials[4], 54);
+    matMap.set(materials[5], 43);
+
+}
