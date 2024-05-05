a9e7e5591377b1944a54f304ea50bae2ffb5ba0c
a1elen <112384717+a1elen@users.noreply.github.com>
2024-05-05 14:53:05 +0800

    added x caves

--- js/map.js
+++ js/map.js
@@ -80,61 +80,115 @@ function generateDirectional() {
         }
     }
 
-    let length = randomRange(10, 20);
-    let roughness = randomRange(1, 100); // 1 to 100
-    let windyness = randomRange(1, 100); // 1 to 100
+    for (let i = 0; i < numberOfYCaves; i++) {
+        let length = randomRange(10, 20);
+        let roughness = randomRange(1, 100); // 1 to 100
+        let windyness = randomRange(1, 100); // 1 to 100
 
-    let startX = randomRange(3, numTiles-3);
-    let startY = numTiles-2;
-    let startWidth = 3;
+        let startX = randomRange(3, numTiles-3);
+        let startY = numTiles-2;
+        let startWidth = 3;
 
+        fillRect(startX-1, startY+1, startX+1, startY-1);
 
-    fillRect(startX-1, startY+1, startX+1, startY-1);
+        let x = startX;
+        let y = startY;
+        let width = startWidth;
+        let maxWidth = 10;
 
-    let x = startX;
-    let y = startY;
-    let width = startWidth;
-    let maxWidth = 10;
+        while (startY - y != length) {
+            y--;
 
-    while (startY - y != length) {
-        y--;
-
-        let randomRoughness;
-        if (randomRange(1, 100) < roughness) {
-            switch(roll(1, 4)) {
-                case 1: randomRoughness = -2; break;
-                case 2: randomRoughness = -1; break;
-                case 3: randomRoughness =  1; break;
-                case 4: randomRoughness =  2; break;
+            let randomRoughness;
+            if (randomRange(1, 100) < roughness) {
+                switch(roll(1, 4)) {
+                    case 1: randomRoughness = -2; break;
+                    case 2: randomRoughness = -1; break;
+                    case 3: randomRoughness =  1; break;
+                    case 4: randomRoughness =  2; break;
+                }
+                width += randomRoughness;
+                if (width < 3) {
+                    width = 3;
+                } else if (width > maxWidth) {
+                    width = maxWidth;
+                }
             }
-            width += randomRoughness;
-            if (width < 3) {
-                width = 3;
-            } else if (width > maxWidth) {
-                width = maxWidth;
+
+            let randomWindyness;
+            if (randomRange(1, 100) < windyness) {
+                switch(roll(1, 4)) {
+                    case 1: randomWindyness = -2; break;
+                    case 2: randomWindyness = -1; break;
+                    case 3: randomWindyness =  1; break;
+                    case 4: randomWindyness =  2; break;
+                }
+                x += randomWindyness;
+                if (x < 0) {
+                    x = 0;
+                } else if (x > numTiles - 3) {
+                    x = numTiles - 3;
+                }
             }
+            fillRect(x, y, x+width, y);
         }
-
-        let randomWindyness;
-        if (randomRange(1, 100) < windyness) {
-            switch(roll(1, 4)) {
-                case 1: randomWindyness = -2; break;
-                case 2: randomWindyness = -1; break;
-                case 3: randomWindyness =  1; break;
-                case 4: randomWindyness =  2; break;
-            }
-            x += randomWindyness;
-            if (x < 0) {
-                x = 0;
-            } else if (x > numTiles - 3) {
-                x = numTiles - 3;
-            }
-        }
-
-        fillRect(x, y, x+width, y);
-
     }
+    
+    for (let i = 0; i < numberOfXCaves; i++) {
+        let length = randomRange(10, 20);
+        let roughness = randomRange(1, 100); // 1 to 100
+        let windyness = randomRange(1, 100); // 1 to 100
 
+        let startX = numTiles+2;
+        let startY = randomRange(3, numTiles-3);
+        let startWidth = 3;
+
+        fillRect(startX-1, startY+1, startX+1, startY-1);
+
+        let x = startX;
+        let y = startY;
+        let width = startWidth;
+        let maxWidth = 10;
+
+        while (x - startX != length) {
+            x++;
+
+            let randomRoughness;
+            if (randomRange(1, 100) < roughness) {
+                switch(roll(1, 4)) {
+                    case 1: randomRoughness = -2; break;
+                    case 2: randomRoughness = -1; break;
+                    case 3: randomRoughness =  1; break;
+                    case 4: randomRoughness =  2; break;
+                }
+                width += randomRoughness;
+                if (width < 3) {
+                    width = 3;
+                } else if (width > maxWidth) {
+                    width = maxWidth;
+                }
+            }
+
+            let randomWindyness;
+            if (randomRange(1, 100) < windyness) {
+                switch(roll(1, 4)) {
+                    case 1: randomWindyness = -2; break;
+                    case 2: randomWindyness = -1; break;
+                    case 3: randomWindyness =  1; break;
+                    case 4: randomWindyness =  2; break;
+                }
+                y += randomWindyness;
+                if (y < 0) {
+                    y = 0;
+                } else if (y > numTiles - 3) {
+                    y = numTiles - 3;
+                }
+            }
+
+            fillRect(x, y, x, y-width);
+
+        }
+    }
 
 }
 
