0eae9360028bdc60d6d454c2539820fcef7c12bb
a1elen <112384717+a1elen@users.noreply.github.com>
2024-05-05 19:34:34 +0800

    bugfix

--- js/items.js
+++ js/items.js
@@ -4,10 +4,12 @@ item = {
     sprite: 0
 }
 
-food = Object.create(item);
-food.type = "food";
-food.hunger = 10;
-food.get = function() {
+items = {}
+
+items.food = Object.create(item);
+items.food.type = "food";
+items.food.hunger = 10;
+items.food.get = function() {
     player.hunger = Math.min(100, player.hunger + this.hunger);
     return true;
 };
