20b686ba778e4974a4374a3383281a3ff718b577
a1elen <112384717+a1elen@users.noreply.github.com>
2024-05-12 16:34:34 +0800

    doors fix

--- js/game.js
+++ js/game.js
@@ -653,10 +653,11 @@ function startLevel(playerHp, playerSpells, randomUpStairs, upOrDown) {
         //let doorTile = randomPassableTile();
         
         let doorTile = shuffle(possibleTiles)[0];
-        
+        for (let i = 0; i < doorsNumber; i++) {
         if (doorTile.constructor.name != "StairsUp" && doorTile.constructor.name != "StairsDown" && doorTile.monster == undefined) {
             doorTile.replace(ClosedDoor);
             possibleTiles.splice(possibleTiles.indexOf(doorTile), 1);
+        }
         }
     }
 
