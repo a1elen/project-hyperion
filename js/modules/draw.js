c9394e51cc4012472ee33783272499b2e9fb2d84
a1elen <112384717+a1elen@users.noreply.github.com>
2024-06-16 12:28:08 +0800

    123

---
+++ js/modules/draw.js
@@ -0,0 +1,17 @@
+draw = {
+    sprite = function (sprite, x, y) {
+        ctx.drawImage(
+            spritesheet,
+            (sprite-(Math.floor(sprite/50)*50))*16,
+            Math.floor(sprite/50)*16,
+            16,
+            16,
+            x*tileSize + shakeX,
+            y*tileSize + shakeY,
+            tileSize,
+            tileSize
+        )
+    }
+}
+
+export {draw};
